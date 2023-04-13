1e98ad1bd7861fe6378db5284b90374a7540bdbd
onelanbin <130176903+onelanbin@users.noreply.github.com>
2023-04-13 18:00:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import anderson_ksamp
 
 
 chat_id = 1266169265 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    p_value = ks_2samp(x, y)[1]
-    if p_value < 0.03:
-        return True
-    else:
-        return False
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    return anderson_ksamp([x, y]).pvalue <= 0.03
 
