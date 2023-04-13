4261128caa0841cfe7a018964a18ce3281d1507e
onelanbin <130176903+onelanbin@users.noreply.github.com>
2023-04-13 17:24:38 +0300

    Task2HYP Done

--- solution.py
+++ solution.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ks_2samp
 
 
 chat_id = 1266169265 # Ваш chat ID, не меняйте название переменной
 
-from scipy.stats import ks_2samp
-
-def test_randomizer(sample1, sample2):
-    p_value = ks_2samp(sample1, sample2)[1]
+def solution(x: np.array, y: np.array) -> bool:
+    p_value = ks_2samp(x, y)[1]
     if p_value < 0.03:
         return True
     else:
         return False
+
